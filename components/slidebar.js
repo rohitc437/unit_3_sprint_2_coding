0c3ace1f866db71b91b89d1f6725060497698818
Rohit <rohitc437>
2021-09-13 14:47:21 +0530

    updated-1

---
+++ components/slidebar.js
@@ -0,0 +1,13 @@
+function navbar(){
+
+    return `
+    <h2><a href="home.html">Home</a></h2>
+    <input type="text" placeholder="search" id="query">
+    <button>Search</button>
+    <div id="options">
+        <h2><a href="news.html">News</a></h2>
+        <h2><a href="search.html">Search</a></h2>
+    </div>`
+}
+
+export {navbar}
